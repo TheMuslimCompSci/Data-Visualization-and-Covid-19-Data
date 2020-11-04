ced2ea57ffacb3e6bceb9c6865f92fe627be4df2
Fareed <k1889155@kcl.ac.uk>
2020-11-04 11:07:45 +0000

    Format CSV

--- src/CumulativeCases.py
+++ src/CumulativeCases.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
-
-
+"""Represents plots from Table 1: Cumulative Number of Cases Tested Positive for COVID-19 - as reported from dates 
+03/07/20 to 07/22/20 on SG website"""
 class CumulativeCases(object):
 
     def __int__(self):
