21ae0260f837b0c932fe312fc9df5d191ccf8483
Fareed <k1889155@kcl.ac.uk>
2021-02-26 21:18:22 +0000

    start comments

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
+# This is the main Python script for the program.
+
+# This provides a simple way of launching the app.
+
+
 import tkinter as tk
 from dashboard import Dashboard
 
-
+# Launch the COVID-19 Data Visualization App
 def launch_app():
     root = tk.Tk()
     Dashboard(root)
     root.mainloop()
 
-
+# Gets run after Shift+F10 is pressed to execute code
 if __name__ == '__main__':
     launch_app()
